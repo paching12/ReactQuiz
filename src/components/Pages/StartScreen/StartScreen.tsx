8c88d4c36a9eaa08a89c15256b4aa3aa24ae4bfa
José Pach <Paching12@icloud.com>
2026-01-02 10:52:29 -0600

    Fix Start screen prop drilling

--- src/components/Pages/StartScreen/StartScreen.tsx
+++ src/components/Pages/StartScreen/StartScreen.tsx
@@ -1,12 +1,24 @@
 import type { FC } from "react";
 import type { StartScreenProp } from ".";
+import { useQuiz } from "../../../QuizContext/QuizContext";
+import { ActionPayloadsTypes } from "../../../shared/actions/actionPayload";
 
-const StartScreen: FC<StartScreenProp> = ({ questions = [], onStart }) => {
+const StartScreen: FC<StartScreenProp> = () => {
+  const { state, dispatch } = useQuiz();
+  const { questions } = state;
+
+  const onStartHandle = () => {
+    dispatch({
+      type: ActionPayloadsTypes.START_QUESTION,
+      payload: undefined,
+    });
+  };
+
   return (
     <div className="start">
       <h2>Welcome to The React Quiz</h2>
       <h3>{questions.length} question to test your React mastery</h3>
-      <button className="btn btn-ui" onClick={onStart}>
+      <button className="btn btn-ui" onClick={onStartHandle}>
         Let's start
       </button>
     </div>
--- src/components/Pages/StartScreen/StartScreen.types.ts
+++
@@ -1,6 +0,0 @@
-import type { question } from "../../../shared/questionTypes";
-
-export type StartScreenProp = {
-  questions: question[];
-  onStart: () => void;
-};
