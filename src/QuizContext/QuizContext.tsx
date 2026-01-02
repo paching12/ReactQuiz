7e8875ec4100d47d26c05cc7ce8ec70c47c2752d
José Pach <Paching12@icloud.com>
2026-01-02 10:53:58 -0600

    Fix QuizzContext

---
+++ src/QuizContext/QuizContext.tsx
@@ -0,0 +1,78 @@
+import { createContext, useContext, useState, type FC } from "react";
+import type {
+  QuizContextProps,
+  QuizContextStateType,
+} from "./QuizContext.types";
+import { useQuizState } from "../hooks/useQuizState";
+import { ActionPayloadsTypes } from "../shared/actions/actionPayload";
+import { initialQuestionState } from "../localReducer/questionStateReducer";
+
+const QuizContext = createContext<QuizContextStateType>({
+  state: initialQuestionState,
+  showRestoreDialog: false,
+  handleRestore: () => {},
+  handleStartNew: () => {},
+  dispatch: () => {},
+  maxPoints: 0,
+});
+
+const QuizProvider: FC<QuizContextProps> = ({ children }) => {
+  const { state, dispatch, clearPersistedState, hasPersistedProgress } =
+    useQuizState();
+
+  const [showRestoreDialog, setShowRestoreDialog] = useState(
+    hasPersistedProgress()
+  );
+
+  const onStartQuiz = () => {
+    dispatch({
+      type: ActionPayloadsTypes.START_QUESTION,
+      payload: undefined,
+    });
+  };
+
+  const handleRestore = () => {
+    setShowRestoreDialog(false);
+    onStartQuiz();
+  };
+
+  const handleStartNew = () => {
+    clearPersistedState();
+    setShowRestoreDialog(false);
+    dispatch({ type: ActionPayloadsTypes.RESET_QUIZ, payload: undefined });
+  };
+
+  // const { question, options, correctOption, points } = questions?.[index] || {};
+
+  const maxPoints = state?.questions?.reduce((acc, curr) => {
+    return acc + curr.points;
+  }, 0);
+
+  return (
+    <QuizContext.Provider
+      value={{
+        state,
+        showRestoreDialog,
+        handleRestore,
+        handleStartNew,
+        maxPoints,
+        dispatch,
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+};
+
+const useQuiz = () => {
+  const context = useContext(QuizContext);
+
+  if (context === undefined) {
+    throw new Error("Quizz context was used outside the QuizProvdier");
+  }
+
+  return context;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { QuizProvider, useQuiz };
---
+++ src/QuizContext/QuizContext.types.ts
@@ -0,0 +1,30 @@
+import type { questionState } from "../localReducer/questionStateReducer";
+import type {
+  ActionPayload,
+  ActionPayloadsTypes,
+} from "../shared/actions/actionPayload";
+import type { question } from "../shared/questionTypes";
+
+export type QuizContextProps = {
+  children: React.ReactNode;
+};
+
+export type Quiz = {
+  questions: question[];
+  status: string;
+  index: number;
+  selectedAnswer: number;
+  currentPoints: number;
+  highscore: number;
+  secondsRemaning: number;
+};
+
+export type QuizContextStateType = {
+  showRestoreDialog: boolean;
+  handleRestore: () => void;
+  handleStartNew: () => void;
+  dispatch: React.Dispatch<ActionPayload<ActionPayloadsTypes>>;
+
+  maxPoints: number;
+  state: questionState;
+};
