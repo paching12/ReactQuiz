d949df98c728c263d353fb1e032790e837be2447
José Pach <Paching12@icloud.com>
2026-01-03 19:47:04 -0600

    Rename css file for TopicItem component adapt topic buttons to mobile

--- src/components/molecules/TopicItem/TopicItem.tsx
+++ src/components/molecules/TopicItem/TopicItem.tsx
@@ -1,6 +1,6 @@
 import { type FC } from "react";
 import type { TopicItemProps } from "./TopicItem.types";
-import "./TopicChoice.css";
+import "./TopicItem.css";
 
 const TopicItem: FC<TopicItemProps> = ({
   title,
